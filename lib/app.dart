6041ea30e7fc417bdced86b31cac18d05d1bfbab
rudarabello <ruda.rabello.silva@gmail.com>
2022-11-08 14:23:57 -0300

    fix: cleaning app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,13 @@
 import 'package:todo_app_flutter/design_system/app_theme.dart';
-import 'package:todo_app_flutter/features/authentication/splash.dart';
-//import 'package:todo_app_flutter/features/home/home_page.dart';
+import 'package:todo_app_flutter/features/splash/splash.dart';
+import 'package:todo_app_flutter/features/home/home_page.dart';
 import 'package:todo_app_flutter/features/login/login_page.dart';
-//import 'package:todo_app_flutter/model/todo_model.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_app_flutter/model/todo_model.dart';
+import 'features/create_todo/create_todo.dart';
 
-//import 'features/create_todo/create_todo.dart';
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +18,15 @@ class MyApp extends StatelessWidget {
       routes: {
         'splash': (context) => const SplashPage(),
         'login': (context) => LoginPage(),
-        // 'home': (context) => const HomePage(title: Todo App),
-        // 'create-todo': (context) => CreateTodo(),
+        'home': (context) => const HomePage(title: 'Rudá Profile'),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == 'create-todo') {
+          return MaterialPageRoute<TodoModel>(
+            builder: (context) => CreateTodo(),
+          );
+        }
+        return null;
       },
     );
   }
